a7f43eb0a6fe4cd05c5bc05256fd39400c7c43b5
Christopher Caston <chris@caston.id.au>
2025-10-17 13:08:37 +0800

    update

--- elm11-firmware/test.lua
+++ elm11-firmware/test.lua
@@ -5,3 +5,9 @@ print("a =", a / 255.0)
 print("b =", b / 255.0)
 result = (a * b) / 255
 print("result =", result / 255.0)
+
+function run_fft_analysis()
+    print("Stochastic REPL Core v0.1")
+    print("Commands: load <reg> <value>, mul, print <reg>, help")
+    print("> ")
+end
